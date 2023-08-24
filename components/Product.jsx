efc98e1882fc03c1dc865a3c1a8c566be1114c58
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-08-24 12:00:32 +0100

    Added little spacing

--- components/Product.jsx
+++ components/Product.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Product = () => {
   return (
-    <div className="lg:p-24 md:px-16 px-8 py-12">
+    <div className="lg:px-20 lg:py-28 md:px-16 px-8 py-12">
         <h1 className="font text-4xl md:text-6xl font-semibold text-center md:mb-20 mb-6">Explore Our Chemical Range</h1>
 
         <div className="flex flex-wrap justify-between md:flex-row items-center">
