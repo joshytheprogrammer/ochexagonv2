6ca6672d61c1b4dbc151f09a1c5e92b8720c17a6
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-08-25 18:17:40 +0100

    Added some changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,12 @@
 "use client"
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '@assets/logo.svg'
 import { useState, useEffect } from 'react';
 
 const Nav = () => {
+  const pathname = usePathname();
 
   const [status, setStatus] = useState('');
 
@@ -29,7 +31,7 @@ const Nav = () => {
   }, []);
 
   return (
-    <div className="lg:px-24 md:px-16 px-8 md:py-8 py-4 flex justify-between items-center relative">
+    <div className="lg:px-24 md:px-16 px-8 md:py-8 py-4 flex justify-between items-center relative ">
       <Link href='/' className="flex items-center">
         <Image
         src={logo}
@@ -42,16 +44,17 @@ const Nav = () => {
     {/* -----------------------Desktop Links---------------------------- */}
 
       <div className="lg:flex items-center  font text-lg hidden">
-        <Link href='/' className="py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900  transition duration-500">
+        <Link href='/' className={`py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900  transition duration-500 ${pathname == "/" ? "border-blue-800 border-b-[2.5px]" : ""}`}>
           Home
         </Link>
-        <Link href='/about' className="ml-6 py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900 transition duration-500">
+
+        <Link href='/about' className={`ml-6 py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900  transition duration-500 ${pathname == "/about" ? "border-blue-800 border-b-[2.5px]" : ""}`}>
           About
         </Link>
-        <Link href='/blog' className="ml-6 py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900 transition duration-500">
+        <Link href='/blog' className={`ml-6 py-1 px-3 relative hover:border-blue-800 hover:border-b-[2.5px] hover:text-blue-900  transition duration-500 ${pathname == "/blog" ? "border-blue-800 border-b-[2.5px]" : ""}`}>
           Blog
         </Link>
-        <Link href='contact' className="ml-6 py-2 px-5 border-primary rounded-[24px] hover:text-white hover:bg-blue-900 transition duration-500">
+        <Link href='/contact' className={`ml-6 py-2 px-5 border-primary rounded-[24px] hover:text-white hover:bg-blue-900 transition duration-500 ${pathname == "/contact" ? "bg-blue-900 text-white" : ""}`}>
           Contact us
         </Link>
       </div>
