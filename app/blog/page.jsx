e9ddd0373deac7f744ed1fd62fc03e139e05ff8a
rapha~El <111363603+RalphFred@users.noreply.github.com>
2023-08-31 11:05:36 +0100

    Made the blog post dynamic

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,38 +1,49 @@
-import Jumbotron from "@components/Jumbotron"
-import Image from "next/image"
-import blog1 from "@assets/blog1.jpg"
-import { blogPostCard } from "@constants"
-import Link from "next/link"
+import Jumbotron from "@components/Jumbotron";
+import Image from "next/image";
+import blog1 from "@assets/blog1.jpg";
+import { blogPostCard } from "@constants";
+import Link from "next/link";
 
 const page = () => {
   return (
     <div className="">
-      <Jumbotron content="Blog" url="https://res.cloudinary.com/dsgvwxygr/image/upload/v1693116985/ochexagon/photo_1_2023-08-27_07-07-22_o5zhe4.jpg"/>
+      <Jumbotron
+        content="Blog"
+        url="https://res.cloudinary.com/dsgvwxygr/image/upload/v1693116985/ochexagon/photo_1_2023-08-27_07-07-22_o5zhe4.jpg"
+      />
 
       <div className="flex justify-between flex-wrap lg:p-24 md:px-16 md:pt-20 md:pb-0 px-8 py-12">
-        {[blogPostCard.map((post) => (
+        {[
+          blogPostCard.map((post) => (
             <div className="w-full md:w-[49%] lg:w-[32%] mb-12 border border-black rounded-xl">
-            <Image 
-            src={blog1}
-            alt="Image"
-            className="w-full h-auto rounded-t-xl"
-            />
-            
-            <div className="p-4">
+              <Image
+                src={blog1}
+                alt="Image"
+                className="w-full h-auto rounded-t-xl"
+              />
+
+              <div className="p-4">
                 <h2 className="font font-bold text-2xl my-2">{post.title}</h2>
-    
+
                 <p className="font text-sm sm:ext-md h-[100px] overflow-hidden">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, at ab temporibus provident ratione error iste amet reiciendis placeat atque eum, facilis, obcaecati doloribus aperiam delectus in sint laboriosam inventore.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Ullam, at ab temporibus provident ratione error iste amet
+                  reiciendis placeat atque eum, facilis, obcaecati doloribus
+                  aperiam delectus in sint laboriosam inventore.
                 </p>
 
-                <button className="bg-black text-white my-2 py-2 px-5 mt-2  rounded-lg font text-base">Read</button>
+                <Link href={`/blog/posts/${post.id}`}>
+                  <button className="bg-black text-white my-2 py-2 px-5 mt-2  rounded-lg font text-base">
+                    Read
+                  </button>
+                </Link>
+              </div>
             </div>
-            </div>
-        ))]
-        }
+          )),
+        ]}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,80 +1,78 @@
-"use client"
-import { useForm } from "react-hook-form"
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from "yup"
+"use client";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-import { collection, addDoc } from 'firebase/firestore';
-import { firestore } from '../firebase/firebase';
+import { collection, addDoc } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
 
-import Input from '@components/Input'
+import Input from "@components/Input";
 
 const schema = yup.object({
-  name: yup
-    .string()
-    .required("Name is a required field"),
+  name: yup.string().required("Name is a required field"),
   email: yup
     .string()
     .required("Email is a required field")
     .email("Email is not valid"),
-  message: yup
-    .string(),
- });
+  message: yup.string(),
+});
 
-  const Contact = () => {
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const { 
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-      } = useForm({
-        resolver: yupResolver(schema)
-      });
-
-      const onSubmit = async (data) => {
-        try {
-          // Rename the 'name' field to 'Name'
-          const modifiedData = {
-            ...data,
-            'Name': data.name, // Rename the 'name' field
-          };
-      
-          // Remove the original 'name' field
-          delete modifiedData.name;
-      
-          // Rename the 'email' field to 'Email'
-          modifiedData['Email'] = modifiedData.email; // Rename the 'email' field
-      
-          // Remove the original 'email' field
-          delete modifiedData.email;
-      
-          // Rename the 'message' field to 'Message Sent'
-          modifiedData['Message Sent'] = modifiedData.message; // Rename the 'message' field
-      
-          // Remove the original 'message' field
-          delete modifiedData.message;
-      
-          // Send the modified form data to Firestore
-          await addDoc(collection(firestore, 'messages'), modifiedData);
-          console.log('Message sent successfully and saved to Firestore:', modifiedData);
-      
-          // Clear the form fields
-          reset();
-          
-          // Optionally, provide user feedback (e.g., show a success message)
-        } catch (error) {
-          console.error('Error sending message and saving to Firestore:', error);
-          // Handle the error gracefully (e.g., show an error message to the user)
-        }
+  const onSubmit = async (data) => {
+    try {
+      // Rename the 'name' field to 'Name'
+      const modifiedData = {
+        ...data,
+        Name: data.name, // Rename the 'name' field
       };
-      
-      
+
+      // Remove the original 'name' field
+      delete modifiedData.name;
+
+      // Rename the 'email' field to 'Email'
+      modifiedData["Email"] = modifiedData.email; // Rename the 'email' field
+
+      // Remove the original 'email' field
+      delete modifiedData.email;
+
+      // Rename the 'message' field to 'Message Sent'
+      modifiedData["Message Sent"] = modifiedData.message; // Rename the 'message' field
+
+      // Remove the original 'message' field
+      delete modifiedData.message;
+
+      // Send the modified form data to Firestore
+      await addDoc(collection(firestore, "messages"), modifiedData);
+      console.log(
+        "Message sent successfully and saved to Firestore:",
+        modifiedData
+      );
+
+      // Clear the form fields
+      reset();
+
+      // Optionally, provide user feedback (e.g., show a success message)
+    } catch (error) {
+      console.error("Error sending message and saving to Firestore:", error);
+      // Handle the error gracefully (e.g., show an error message to the user)
+    }
+  };
 
   return (
     <div className="lg:px-24 lg:pt-20 md:px-16 md px-8 py-12 flex lg:flex-row  justify-between flex-col">
       <div className="lg:w-2/5 w-full mr-12 mb-20">
         <p className="font text-lg">
-          If you have any questions or comments, please contact us via email, or send us a message using the contact form.
+          If you have any questions or comments, please contact us via email, or
+          send us a message using the contact form.
         </p>
 
         <div>
@@ -86,40 +84,43 @@ const schema = yup.object({
           <h2 className="font text-2xl font-medium pt-8">Phone No</h2>
           <span className="font text-lg">+234-803-0512-882</span>
         </div>
-        
       </div>
 
       <form onSubmit={handleSubmit(onSubmit)} className="lg:w-3/5 w-full">
-        <Input 
-        id="Username"
-        label="Username"
-        type="text"
-        register={{ ...register("name") }}
-        errorMessage={errors.name?.message}
+        <Input
+          id="Username"
+          label="Username"
+          type="text"
+          register={{ ...register("name") }}
+          errorMessage={errors.name?.message}
         />
-        <Input 
-        id="Email"
-        label="Email"
-        type="email"
-        register={{ ...register("email") }} 
-        errorMessage={errors.email?.message}
+        <Input
+          id="Email"
+          label="Email"
+          type="email"
+          register={{ ...register("email") }}
+          errorMessage={errors.email?.message}
         />
 
         <div className="flex flex-col mb-4">
           <label className="font text-xl mb-2">Message</label>
-          <textarea 
-          name="message" 
-          id="Message" 
-          cols="30" 
-          rows="10"
-          {...register("message")} 
-          className="border-2 w-full h-[180px] p-3 rounded-xl font focus:outline-none focus:border-slate-300 focus:ring-1 focus:ring-slate-300"></textarea>
+          <textarea
+            name="message"
+            id="Message"
+            cols="30"
+            rows="10"
+            {...register("message")}
+            className="border-2 w-full h-[180px] p-3 rounded-xl font focus:outline-none focus:border-slate-300 focus:ring-1 focus:ring-slate-300"
+          ></textarea>
         </div>
 
-        <input type="submit" className="block font text-xl w-full text-center p-3 background-primary cursor-pointer text-secondary rounded-xl"/>
+        <input
+          type="submit"
+          className="block font text-xl w-full text-center p-3 background-primary cursor-pointer text-secondary rounded-xl"
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
